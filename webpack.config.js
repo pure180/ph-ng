dfcdc332b12836d5b6aec3ff07a0e918d87bf1a4
André Gaul <andre@gaul.io>
2017-04-16 17:41:06 +0200

    webpack: cleanup ejected config, add html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,35 +13,21 @@ const { AotPlugin } = require('@ngtools/webpack');
 
 const nodeModules = path.join(process.cwd(), 'node_modules');
 const entryPoints =  ['inline', 'polyfills', 'sw-register', 'styles', 'vendor', 'main'];
-const baseHref = '';
-const deployUrl = '';
 
+const production = process.env.NODE_ENV === 'production';
 
-
-
-module.exports = {
-  devtool: false,
+const config = {
   resolve: {
     extensions:  ['.ts', '.js'],
-    modules: [
-      './node_modules'
-    ]
+    modules: ['./node_modules']
   },
   resolveLoader: {
-    modules: [
-      './node_modules'
-    ]
+    modules: ['./node_modules']
   },
   entry: {
-    main: [
-      './src/main.ts'
-    ],
-    polyfills: [
-      './src/polyfills.ts'
-    ],
-    styles: [
-      './src/styles.scss'
-    ]
+    main: ['./src/main.ts'],
+    polyfills: ['./src/polyfills.ts'],
+    styles: ['./src/styles.scss']
   },
   output: {
     path: path.join(process.cwd(), 'dist'),
@@ -64,7 +50,7 @@ module.exports = {
       },
       {
         test: /\.html$/,
-        loader: 'raw-loader'
+        loader: 'html-loader'
       },
       {
         test: /\.(eot|svg)$/,
@@ -75,21 +61,8 @@ module.exports = {
         loader: 'url-loader?name=[name].[hash:20].[ext]&limit=10000'
       },
       {
-        exclude: [
-          path.join(process.cwd(), 'src/styles.scss')
-        ],
-        test: /\.css$/,
-        loaders: [
-          'exports-loader?module.exports.toString()',
-          'css-loader?{\"sourceMap\":false,\"importLoaders\":1}',
-          'postcss-loader'
-        ]
-      },
-      {
-        exclude: [
-          path.join(process.cwd(), 'src/styles.scss')
-        ],
-        test: /\.scss$|\.sass$/,
+        exclude: [path.join(process.cwd(), 'src/styles.scss')],
+        test: /\.scss$/,
         loaders: [
           'exports-loader?module.exports.toString()',
           'css-loader?{\"sourceMap\":false,\"importLoaders\":1}',
@@ -98,83 +71,13 @@ module.exports = {
         ]
       },
       {
-        exclude: [
-          path.join(process.cwd(), 'src/styles.scss')
-        ],
-        test: /\.less$/,
-        loaders: [
-          'exports-loader?module.exports.toString()',
-          'css-loader?{\"sourceMap\":false,\"importLoaders\":1}',
-          'postcss-loader',
-          'less-loader'
-        ]
-      },
-      {
-        exclude: [
-          path.join(process.cwd(), 'src/styles.scss')
-        ],
-        test: /\.styl$/,
-        loaders: [
-          'exports-loader?module.exports.toString()',
-          'css-loader?{\"sourceMap\":false,\"importLoaders\":1}',
-          'postcss-loader',
-          'stylus-loader?{\"sourceMap\":false,\"paths\":[]}'
-        ]
-      },
-      {
-        include: [
-          path.join(process.cwd(), 'src/styles.scss')
-        ],
-        test: /\.css$/,
-        loaders: ExtractTextPlugin.extract({
-          use: [
-            'css-loader?{\"sourceMap\":false,\"importLoaders\":1}',
-            'postcss-loader'
-          ],
-          fallback: 'style-loader',
-          publicPath: ''
-        })
-      },
-      {
-        include: [
-          path.join(process.cwd(), 'src/styles.scss')
-        ],
-        test: /\.scss$|\.sass$/,
+        include: [path.join(process.cwd(), 'src/styles.scss')],
+        test: /\.scss$/,
         loaders: ExtractTextPlugin.extract({
           use: [
             'css-loader?{\"sourceMap\":false,\"importLoaders\":1}',
             'postcss-loader',
             'sass-loader'
-          ],
-          fallback: 'style-loader',
-          publicPath: ''
-        })
-      },
-      {
-        include: [
-          path.join(process.cwd(), 'src/styles.scss')
-        ],
-        test: /\.less$/,
-        loaders: ExtractTextPlugin.extract({
-          use: [
-            'css-loader?{\"sourceMap\":false,\"importLoaders\":1}',
-            'postcss-loader',
-            'less-loader'
-          ],
-          fallback: 'style-loader',
-          publicPath: ''
-        })
-      },
-      {
-        include: [
-          path.join(process.cwd(), 'src/styles.scss')
-        ],
-        test: /\.styl$/,
-        loaders: ExtractTextPlugin.extract({
-          use: [
-            'css-loader?{\"sourceMap\":false,\"importLoaders\":1}',
-            'postcss-loader',
-            'stylus-loader?{\"sourceMap\":false,\"paths\":[]}'
           ],
           fallback: 'style-loader',
           publicPath: ''
@@ -188,18 +91,8 @@ module.exports = {
   },
   plugins: [
     new NoEmitOnErrorsPlugin(),
-    new GlobCopyWebpackPlugin({
-      patterns: [
-        'assets',
-        'favicon.ico'
-      ],
-      globOptions: {
-        cwd: '/home/andre/git-repos/paperhive-frontend-ng/src',
-        dot: true,
-        ignore: '**/.gitkeep'
-      }
-    }),
     new ProgressPlugin(),
+    new SuppressExtractedTextChunksWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: './index.html',
@@ -244,61 +137,10 @@ module.exports = {
       filename: '[name].[contenthash:20].bundle.css',
       disable: false
     }),
-    new LoaderOptionsPlugin({
-      sourceMap: false,
-      options: {
-        postcss: [
-          autoprefixer(),
-          postcssUrl({url: (URL) => {
-            // Only convert root relative URLs, which CSS-Loader won't process into require().
-            if (!URL.startsWith('/') || URL.startsWith('//')) {
-                return URL;
-            }
-            if (deployUrl.match(/:\/\//)) {
-                // If deployUrl contains a scheme, ignore baseHref use deployUrl as is.
-                return `${deployUrl.replace(/\/$/, '')}${URL}`;
-            }
-            else if (baseHref.match(/:\/\//)) {
-                // If baseHref contains a scheme, include it as is.
-                return baseHref.replace(/\/$/, '') +
-                    `/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
-            }
-            else {
-                // Join together base-href, deploy-url and the original URL.
-                // Also dedupe multiple slashes into single ones.
-                return `/${baseHref}/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
-            }
-        }}),
-          cssnano({ safe: true, autoprefixer: false })
-        ],
-        sassLoader: {
-          sourceMap: false,
-          includePaths: []
-        },
-        lessLoader: {
-          sourceMap: false
-        },
-        context: ''
-      }
-    }),
-    new SuppressExtractedTextChunksWebpackPlugin(),
-    new DefinePlugin({
-      'process.env.NODE_ENV': '\"production\"'
-    }),
     new HashedModuleIdsPlugin({
       hashFunction: 'md5',
       hashDigest: 'base64',
       hashDigestLength: 4
-    }),
-    new UglifyJsPlugin({
-      mangle: {
-        screw_ie8: true
-      },
-      compress: {
-        screw_ie8: true,
-        warnings: false
-      },
-      sourceMap: false
     }),
     new AotPlugin({
       mainPath: 'main.ts',
@@ -321,3 +163,25 @@ module.exports = {
     setImmediate: false
   }
 };
+
+if (production) {
+  config.devtool = false;
+
+  config.plugins.push(...[
+    new DefinePlugin({
+      'process.env.NODE_ENV': '\"production\"'
+    }),
+    new UglifyJsPlugin({
+      mangle: {
+        screw_ie8: true
+      },
+      compress: {
+        screw_ie8: true,
+        warnings: false
+      },
+      sourceMap: false
+    }),
+  ]);
+}
+
+module.exports = config;
